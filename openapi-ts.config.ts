398f2ce8db4b456d39a8bee07b7391b270fc7967
Péricles Pires <periclespires@protonmail.com>
2026-03-04 19:50:46 -0300

    feat: add payments api

--- openapi-ts.config.ts
+++ openapi-ts.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from '@hey-api/openapi-ts';
 
 export default defineConfig({
-  input: ['https://hype.byal.org/tickets/docs-json', 'https://hype.byal.org/auth/docs-json'],
-  output: ['src/_gen/api/tickets', 'src/_gen/api/auth'],
+  input: [
+    'https://hype.byal.org/tickets/docs-json',
+    'https://hype.byal.org/auth/docs-json',
+    'https://hype.byal.org/payment/docs-json',
+  ],
+  output: ['src/_gen/api/tickets', 'src/_gen/api/auth', 'src/_gen/api/payment'],
   plugins: ['@hey-api/client-fetch'],
 });
